f3581a1b1fb08ac90a9966c85b3e957237f7a3d4
danhagen <danhagen@users.noreply.github.com>
2016-12-15 11:39:13 -0800

    Adding to Index!

--- add_to_index.bat
+++ add_to_index.bat
@@ -1,8 +1,8 @@
-ipython IndexInput.py
-pdflatex main.tex >nul 2>nul
-git add . 
-git commit --quiet -m "Adding to Index!" 
-git push --quiet origin master
-danhagen
-/Vikings84
-
+echo off
+echo.
+ ipython IndexInput.py
+ pdflatex main.tex >nul 2>nul
+ git add . 
+ git commit --quiet -m "Adding to Index!" 
+ git push --quiet origin master
+echo. 
