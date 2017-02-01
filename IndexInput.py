35e64293abf23b39ebef816933d263d9b18ed599
danhagen <danhagen@users.noreply.github.com>
2017-02-01 12:48:23 -0800

    Adding to Index!

--- IndexInput.py
+++ IndexInput.py
@@ -549,7 +549,7 @@ def options(index):
 # 			exit_prompt=True
 def index_from_letter(letters,index):
 	for letter in letters:
-		latexfile.write("\\underline{\\textit{"+letter+"\;}} \\\\")
+		latexfile.write("\\underline{\\textit{"+letter+"\\hspace{10em}}} \\\\")
 		for key in sorted(index.keys()):
 			if key[0] == letter: latexfile.write(index[key].print_topic())
 			
