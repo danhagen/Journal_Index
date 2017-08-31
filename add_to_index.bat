8133fef6fcc3958dd3a6bd5395d576e191323c65
dhagen.work <dhagen@usc.edu>
2017-08-31 08:07:04 -0700

    Adding to Index!

--- add_to_index.bat
+++ add_to_index.bat
@@ -1,7 +1,7 @@
 @echo off
  git pull --quiet origin master
  ipython IndexInput.py
- pdflatex main.tex >nul 2>nul
- git add . 
- git commit --quiet -m "Adding to Index!" 
+ pdflatex CompleteIndex.tex >nul 2>nul
+ git add .
+ git commit --quiet -m "Adding to Index!"
  git push --quiet origin master
